7064f9ff8035b3ea52835fec54d9ff85890e4e4b
Mario <mario.korth@rub.de>
2017-08-27 13:00:16 +0200

    replacing duplicate code

--- link/wrapper.php
+++ link/wrapper.php
@@ -58,62 +58,9 @@ $operation = readVar("operation", array("read", "write", "execute"));
 	</table>
 	<script>
 
-
-	function sleep(timeout) {
-		return new Promise(resolve => setTimeout(resolve, timeout));
-	}
-
-	function call(func, args = []) {
-		if(!document.callQueue) {
-			document.callQueue = Array();
-		}
-		document.callQueue.push([func, args]); 
-	}
-
-	/* This solution only works for javascript so far. The protocol could be set with a PHP variable. */
-	/* Maybe modify existing code such that every test case sets free = true in its last line by default */
-	async function depleteQueue() { /* async: allows await to be used */
-		if(!document.working || document.working == false) {
-			document.working = true;
-		}
-		else {
-			return 0;
-		}
-		var func;
-		if(!document.free) {
-			document.free = true;
-		}
-		var i = 0;
-		while (i < document.callQueue.length) {
-			if (document.callQueue[i]) { /* function name */
- 				var code = document.callQueue[i][0].toString(); /* get source code */
-				while (document.free == false)
-				{
-					await sleep(10);
-				}
-				document.free = false;
-				/* The following line should be set according to the execution that is preferred via PHP */
-				<?php 
-				if (isset($_GET['exec']) && $_GET['exec'] === 'js') {
-					echo 'window.location = "javascript:" + code + document.callQueue[i][0].name + ".apply(null, document.callQueue[" + i + "][1]);";';
-					echo "\n";
-				} else {
-					echo 'document.callQueue[i][0].apply(null, document.callQueue[i][1]);';
-					echo "\n";
-				}
-				?>
-				while (document.free == false) /* wait to clean the queue */
-				{
-					await sleep(10);
-				}
-				document.callQueue[i] = undefined; /* clean current queue entry. This still bloats a bit but seems to be the best solution so far */
-			}
-			i++;
-			
-		}
-		document.working = false;
-	}
-
+	<?php 
+	include(__DIR__ . "/../util/call_stack.php");
+	?>
 
 
 	function set(id, value, info) {
